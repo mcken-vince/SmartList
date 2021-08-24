7251168a27f43b8fd18f37bbcd882c24a989d7ee
Lliam McBean <lliam.mcbean@gmail.com>
2021-08-24 01:36:48 +0000

    convert callbacks to promises

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,29 +1,32 @@
 $(() => {
   const appendListElements = () => {
-    $.get('/reminders', (data) => {
-      for (const item of data['reminders']) {
-        switch(item['type_id']) {
-          case 1:
-            $('.movies').append(`<li>${item.name}</li>`);
-            break;
-          case 2:
-            $('.books').append(`<li>${item.name}</li>`);
-            break;
-          case 3:
-            $('.foods').append(`<li>${item.name}</li>`);
-            break;
-          case 4: 
-            $('.products').append(`<li>${item.name}</li>`);
-            break;
+    $.get('/reminders')
+      .then((data) => {
+        for (const item of data['reminders']) {
+          switch(item['type_id']) {
+            case 1:
+              $('.movies').append(`<li>${item.name}</li>`);
+              break;
+            case 2:
+              $('.books').append(`<li>${item.name}</li>`);
+              break;
+            case 3:
+              $('.foods').append(`<li>${item.name}</li>`);
+              break;
+            case 4: 
+              $('.products').append(`<li>${item.name}</li>`);
+              break;
+          }
         }
-      }
-    })
+      })
+      .catch((err) => {
+        console.log('error message: ', err)
+      })
   };
 
   $('#new-item').on('submit', function(event) {
     event.preventDefault();
     let $userSubmission = $('.list-input-field').val();
-    $userSubmission = JSON.stringify($userSubmission)
 
     $.post('/reminders', $userSubmission, appendListElements())
   })
