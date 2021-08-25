a0a566c2160f653dc8142c78dfb86ff1e38f0d62
Drew Kang <kangdrew95@gmail.com>
2021-08-25 17:30:51 +0000

    Resolved issue with modal and delete conflict. Removed modal.js

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -41,6 +41,21 @@ $(() => {
     });
   });
 
+
+  $("ul").on("click",'li', function() {
+    const id = $(this).attr("class").split("-")[1];
+
+    $.ajax({
+      url: `/reminders/${id}`,
+      type: 'GET',
+      success: (response) => {
+        console.log(response.results);
+      }
+    });
+
+  });
+
+
   $('#new-item').on('submit', function(event) {
     event.preventDefault();
     $(this).css('display', 'none');
