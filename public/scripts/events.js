a292ee1018c1ba06577e009f174c27e336915a2f
Vincent McKen <mcken.vince@gmail.com>
2021-08-26 15:49:28 +0000

    add click event listener for list h3, expand the clicked list and hide all others, reverse when clicked again

--- public/scripts/events.js
+++ public/scripts/events.js
@@ -12,7 +12,7 @@ $(()=> {
     }
     console.log($('#new-item').attr('action'))
   });
-  
+
   $(".fa-shopping-cart").on("click", () => {
     if ($('#new-item').attr('action') !== '/products') {
       $('#new-item').attr('action', '/products');
@@ -43,4 +43,20 @@ $(()=> {
   $(".fa-times").on("click", () => {
     $('.modal-container').css('display', 'none')
   });
+
+  // enlarge individual list and hide others on click
+  $(".dumbledore h3").on("click", (event) => {
+    const $allContainers = $('.dumbledore');
+    console.log($allContainers);
+
+    const $container = $(event.target).parent()
+    if ($container.hasClass('select-list')) {
+      $allContainers.css('display', 'block');
+      $container.removeClass('select-list');
+    } else {
+      $allContainers.css('display', 'none');
+      $container.css('display', 'block');
+      $container.addClass('select-list');
+    }
+  })
 })
